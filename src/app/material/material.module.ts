609d07d4cea6f8769a68d7c58e0263d42ad7db5f
calba <alex.calbajos@gmail.com>
2018-07-03 11:18:59 +0300

    test change

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,7 +2,15 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule, MatCheckboxModule, MatIconModule } from '@angular/material';
 
 @NgModule({
-    imports: [MatButtonModule, MatCheckboxModule, MatIconModule],
-    exports: [MatButtonModule, MatCheckboxModule, MatIconModule]
+    imports: [
+        MatButtonModule,
+        MatCheckboxModule,
+        MatIconModule
+    ],
+    exports: [
+        MatButtonModule,
+        MatCheckboxModule,
+        MatIconModule
+    ]
 })
 export class MaterialModule { }
